93add8e71fac75e3599c19730f7566b5e7c20abf
Kiaresko <kseniia.i@ideasoft.io>
2019-12-04 10:48:43 +0200

    Updates

---
+++ precache-manifest.37351fff41c25767964dacc7a8834a91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9797b76b5a5da0baa6402a8ef21fb1e",
+    "url": "/cra-hw-6/index.html"
+  },
+  {
+    "revision": "8376fec8d4bf68b1cd4f",
+    "url": "/cra-hw-6/static/css/main.9f4e3ac2.chunk.css"
+  },
+  {
+    "revision": "72bbcd84e0f486e66700",
+    "url": "/cra-hw-6/static/js/2.b566e203.chunk.js"
+  },
+  {
+    "revision": "8376fec8d4bf68b1cd4f",
+    "url": "/cra-hw-6/static/js/main.1ef5f77f.chunk.js"
+  },
+  {
+    "revision": "afab06d8ccd1d9170d74",
+    "url": "/cra-hw-6/static/js/runtime-main.f3f8a06c.js"
+  }
+]);
